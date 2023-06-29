89d8f479e8a74605a3f80cbcf7ceb39a46aae91c
Marvin Ferber <ferbhome@googlemail.com>
2023-06-29 21:08:31 +0200

    Score added

--- main.py
+++ main.py
@@ -1,37 +1,55 @@
 def my_function():
     global y22
-    display.clear()
-    gameField()
-    if y22 < 7 and A[x22][y22 + 1] != "#":
-        y22 += 1
-    else:
-        GAME_ZIP64.run_motor(50)
-    pixelAt(x22, y22)
-    testWin()
+    if running and not (finished):
+        display.clear()
+        gameField()
+        if y22 < 7 and A[x22][y22 + 1] != "#":
+            y22 += 1
+        else:
+            GAME_ZIP64.run_motor(50)
+        pixelAt(x22, y22)
+        testWin()
 GAME_ZIP64.on_button_press(GAME_ZIP64.ZIP64ButtonPins.DOWN,
     GAME_ZIP64.ZIP64ButtonEvents.DOWN,
     my_function)
+
+def my_function2():
+    global running
+    running = True
+GAME_ZIP64.on_button_press(GAME_ZIP64.ZIP64ButtonPins.FIRE1,
+    GAME_ZIP64.ZIP64ButtonEvents.DOWN,
+    my_function2)
+
+def my_function3():
+    global running
+    running = True
+GAME_ZIP64.on_button_press(GAME_ZIP64.ZIP64ButtonPins.FIRE2,
+    GAME_ZIP64.ZIP64ButtonEvents.DOWN,
+    my_function3)
 
 def pixelAt(x: number, y: number):
     display.set_matrix_color(x, y, GAME_ZIP64.colors(ZipLedColors.YELLOW))
     display.show()
 def testWin():
+    global hit
     if A[x22][y22] == "A":
         music.play_melody("C D E F G A B C5 ", 240)
+        hit += 1
 
-def my_function2():
+def my_function4():
     global x22
-    display.clear()
-    gameField()
-    if x22 > 0 and A[x22 - 1][y22] != "#":
-        x22 += -1
-    else:
-        GAME_ZIP64.run_motor(50)
-    pixelAt(x22, y22)
-    testWin()
+    if running and not (finished):
+        display.clear()
+        gameField()
+        if x22 > 0 and A[x22 - 1][y22] != "#":
+            x22 += -1
+        else:
+            GAME_ZIP64.run_motor(50)
+        pixelAt(x22, y22)
+        testWin()
 GAME_ZIP64.on_button_press(GAME_ZIP64.ZIP64ButtonPins.LEFT,
     GAME_ZIP64.ZIP64ButtonEvents.DOWN,
-    my_function2)
+    my_function4)
 
 def gameField():
     for i in range(8):
@@ -44,37 +62,44 @@ def gameField():
                     display.set_matrix_color(i, j, GAME_ZIP64.colors(ZipLedColors.GREEN))
     display.show()
 
-def my_function3():
+def my_function5():
     global x22
-    display.clear()
-    gameField()
-    if x22 < 7 and A[x22 + 1][y22] != "#":
-        x22 += 1
-    else:
-        GAME_ZIP64.run_motor(50)
-    pixelAt(x22, y22)
-    testWin()
+    if running and not (finished):
+        display.clear()
+        gameField()
+        if x22 < 7 and A[x22 + 1][y22] != "#":
+            x22 += 1
+        else:
+            GAME_ZIP64.run_motor(50)
+        pixelAt(x22, y22)
+        testWin()
 GAME_ZIP64.on_button_press(GAME_ZIP64.ZIP64ButtonPins.RIGHT,
     GAME_ZIP64.ZIP64ButtonEvents.DOWN,
-    my_function3)
+    my_function5)
 
-def my_function4():
+def my_function6():
     global y22
-    display.clear()
-    gameField()
-    if y22 > 0 and A[x22][y22 - 1] != "#":
-        y22 += -1
-    else:
-        GAME_ZIP64.run_motor(50)
-    pixelAt(x22, y22)
-    testWin()
+    if running and not (finished):
+        display.clear()
+        gameField()
+        if y22 > 0 and A[x22][y22 - 1] != "#":
+            y22 += -1
+        else:
+            GAME_ZIP64.run_motor(50)
+        pixelAt(x22, y22)
+        testWin()
 GAME_ZIP64.on_button_press(GAME_ZIP64.ZIP64ButtonPins.UP,
     GAME_ZIP64.ZIP64ButtonEvents.DOWN,
-    my_function4)
+    my_function6)
 
 y2 = 0
 x2 = 0
 done = False
+hit = 0
+finished = False
+running = False
+y22 = 0
+x22 = 0
 display: GAME_ZIP64.ZIP64Display = None
 A: List[List[str]] = []
 A = [["#", "#", "#", "#", "#", "#", "#", "#"],
@@ -93,24 +118,84 @@ display.set_brightness(20)
 display.clear()
 x22 = 4
 y22 = 4
-gameField()
-pixelAt(x22, y22)
+cx = 4
+cy = 4
 
 def on_forever():
-    global x2, y2, x22, y22, done
+    global done, x2, y2
     basic.pause(2000)
-    for k in range(8):
-        for l in range(8):
-            if A[k][l] == "A":
-                A[k][l] = " "
-    done = False
-    while not (done):
-        x2 = randint(0, 7)
-        y2 = randint(0, 7)
-        if (not A[x2][y2] == "#") and x2 != x22 and y2 != y22:
-            A[x2][y2] = "A"
-            done = True
-    display.clear()
+    if not (running) or finished:
+        if not (finished):
+            display.clear()
+            display.set_matrix_color(7, 5, GAME_ZIP64.colors(ZipLedColors.WHITE))
+            display.set_matrix_color(6, 5, GAME_ZIP64.colors(ZipLedColors.WHITE))
+            display.set_matrix_color(5, 5, GAME_ZIP64.colors(ZipLedColors.WHITE))
+            display.set_matrix_color(4, 5, GAME_ZIP64.colors(ZipLedColors.WHITE))
+            display.set_matrix_color(3, 5, GAME_ZIP64.colors(ZipLedColors.WHITE))
+            display.set_matrix_color(6, 6, GAME_ZIP64.colors(ZipLedColors.WHITE))
+            display.set_matrix_color(6, 4, GAME_ZIP64.colors(ZipLedColors.WHITE))
+            display.set_matrix_color(5, 7, GAME_ZIP64.colors(ZipLedColors.WHITE))
+            display.set_matrix_color(5, 3, GAME_ZIP64.colors(ZipLedColors.WHITE))
+            display.show()
+        else:
+            display.clear()
+            display.show()
+            basic.pause(5000)
+            control.reset()
+    else:
+        for k in range(8):
+            for l in range(8):
+                if A[k][l] == "A":
+                    A[k][l] = " "
+        done = False
+        while not (done):
+            x2 = randint(0, 7)
+            y2 = randint(0, 7)
+            if not (A[x2][y2] == "#") and x2 != x22 and y2 != y22:
+                A[x2][y2] = "A"
+                done = True
+        display.clear()
+        gameField()
+        pixelAt(x22, y22)
+basic.forever(on_forever)
+
+def on_in_background():
+    global cx, cy, finished
+    while not (running):
+        basic.show_icon(IconNames.GHOST)
+        basic.pause(200)
+    basic.show_leds("""
+        . . . . .
+        . . . . .
+        . # # # .
+        # . # . #
+        # # # # #
+        """)
+    basic.pause(500)
+    basic.show_leds("""
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . # # # .
+        """)
+    basic.pause(500)
+    basic.show_leds("""
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        """)
     gameField()
     pixelAt(x22, y22)
-basic.forever(on_forever)
+    while cx >= 0 and cy >= 0:
+        led.plot(cx, cy)
+        basic.pause(1000)
+        cx += -1
+        if cx < 0:
+            cx = 4
+            cy += -1
+    finished = True
+    basic.show_number(hit)
+control.in_background(on_in_background)
--- main.ts
+++ main.ts
@@ -1,13 +1,21 @@
 GAME_ZIP64.onButtonPress(GAME_ZIP64.ZIP64ButtonPins.Down, GAME_ZIP64.ZIP64ButtonEvents.Down, function () {
-    display.clear()
-    gameField()
-    if (y22 < 7 && A[x22][y22 + 1] != "#") {
-        y22 += 1
-    } else {
-        GAME_ZIP64.runMotor(50)
+    if (running && !(finished)) {
+        display.clear()
+        gameField()
+        if (y22 < 7 && A[x22][y22 + 1] != "#") {
+            y22 += 1
+        } else {
+            GAME_ZIP64.runMotor(50)
+        }
+        pixelAt(x22, y22)
+        testWin()
     }
-    pixelAt(x22, y22)
-    testWin()
+})
+GAME_ZIP64.onButtonPress(GAME_ZIP64.ZIP64ButtonPins.Fire1, GAME_ZIP64.ZIP64ButtonEvents.Down, function () {
+    running = true
+})
+GAME_ZIP64.onButtonPress(GAME_ZIP64.ZIP64ButtonPins.Fire2, GAME_ZIP64.ZIP64ButtonEvents.Down, function () {
+    running = true
 })
 function pixelAt (x: number, y: number) {
     display.setMatrixColor(x, y, GAME_ZIP64.colors(ZipLedColors.Yellow))
@@ -16,18 +24,21 @@ function pixelAt (x: number, y: number) {
 function testWin () {
     if (A[x22][y22] == "A") {
         music.playMelody("C D E F G A B C5 ", 240)
+        hit += 1
     }
 }
 GAME_ZIP64.onButtonPress(GAME_ZIP64.ZIP64ButtonPins.Left, GAME_ZIP64.ZIP64ButtonEvents.Down, function () {
-    display.clear()
-    gameField()
-    if (x22 > 0 && A[x22 - 1][y22] != "#") {
-        x22 += -1
-    } else {
-        GAME_ZIP64.runMotor(50)
+    if (running && !(finished)) {
+        display.clear()
+        gameField()
+        if (x22 > 0 && A[x22 - 1][y22] != "#") {
+            x22 += -1
+        } else {
+            GAME_ZIP64.runMotor(50)
+        }
+        pixelAt(x22, y22)
+        testWin()
     }
-    pixelAt(x22, y22)
-    testWin()
 })
 function gameField () {
     for (let i = 0; i <= 7; i++) {
@@ -45,30 +56,37 @@ function gameField () {
     display.show()
 }
 GAME_ZIP64.onButtonPress(GAME_ZIP64.ZIP64ButtonPins.Right, GAME_ZIP64.ZIP64ButtonEvents.Down, function () {
-    display.clear()
-    gameField()
-    if (x22 < 7 && A[x22 + 1][y22] != "#") {
-        x22 += 1
-    } else {
-        GAME_ZIP64.runMotor(50)
+    if (running && !(finished)) {
+        display.clear()
+        gameField()
+        if (x22 < 7 && A[x22 + 1][y22] != "#") {
+            x22 += 1
+        } else {
+            GAME_ZIP64.runMotor(50)
+        }
+        pixelAt(x22, y22)
+        testWin()
     }
-    pixelAt(x22, y22)
-    testWin()
 })
 GAME_ZIP64.onButtonPress(GAME_ZIP64.ZIP64ButtonPins.Up, GAME_ZIP64.ZIP64ButtonEvents.Down, function () {
-    display.clear()
-    gameField()
-    if (y22 > 0 && A[x22][y22 - 1] != "#") {
-        y22 += -1
-    } else {
-        GAME_ZIP64.runMotor(50)
+    if (running && !(finished)) {
+        display.clear()
+        gameField()
+        if (y22 > 0 && A[x22][y22 - 1] != "#") {
+            y22 += -1
+        } else {
+            GAME_ZIP64.runMotor(50)
+        }
+        pixelAt(x22, y22)
+        testWin()
     }
-    pixelAt(x22, y22)
-    testWin()
 })
 let y2 = 0
 let x2 = 0
 let done = false
+let hit = 0
+let finished = false
+let running = false
 let y22 = 0
 let x22 = 0
 let display: GAME_ZIP64.ZIP64Display = null
@@ -163,27 +181,90 @@ display.setBrightness(20)
 display.clear()
 x22 = 4
 y22 = 4
-gameField()
-pixelAt(x22, y22)
+let cx = 4
+let cy = 4
 basic.forever(function () {
     basic.pause(2000)
-    for (let k = 0; k <= 7; k++) {
-        for (let l = 0; l <= 7; l++) {
-            if (A[k][l] == "A") {
-                A[k][l] = " "
+    if (!(running) || finished) {
+        if (!(finished)) {
+            display.clear()
+            display.setMatrixColor(7, 5, GAME_ZIP64.colors(ZipLedColors.White))
+            display.setMatrixColor(6, 5, GAME_ZIP64.colors(ZipLedColors.White))
+            display.setMatrixColor(5, 5, GAME_ZIP64.colors(ZipLedColors.White))
+            display.setMatrixColor(4, 5, GAME_ZIP64.colors(ZipLedColors.White))
+            display.setMatrixColor(3, 5, GAME_ZIP64.colors(ZipLedColors.White))
+            display.setMatrixColor(6, 6, GAME_ZIP64.colors(ZipLedColors.White))
+            display.setMatrixColor(6, 4, GAME_ZIP64.colors(ZipLedColors.White))
+            display.setMatrixColor(5, 7, GAME_ZIP64.colors(ZipLedColors.White))
+            display.setMatrixColor(5, 3, GAME_ZIP64.colors(ZipLedColors.White))
+            display.show()
+        } else {
+            display.clear()
+            display.show()
+            basic.pause(5000)
+            control.reset()
+        }
+    } else {
+        for (let k = 0; k <= 7; k++) {
+            for (let l = 0; l <= 7; l++) {
+                if (A[k][l] == "A") {
+                    A[k][l] = " "
+                }
             }
         }
-    }
-    done = false
-    while (!(done)) {
-        x2 = randint(0, 7)
-        y2 = randint(0, 7)
-        if (!(A[x2][y2] == "#") && x2 != x22 && y2 != y22) {
-            A[x2][y2] = "A"
-            done = true
+        done = false
+        while (!(done)) {
+            x2 = randint(0, 7)
+            y2 = randint(0, 7)
+            if (!(A[x2][y2] == "#") && x2 != x22 && y2 != y22) {
+                A[x2][y2] = "A"
+                done = true
+            }
         }
+        display.clear()
+        gameField()
+        pixelAt(x22, y22)
     }
-    display.clear()
+})
+control.inBackground(function () {
+    while (!(running)) {
+        basic.showIcon(IconNames.Ghost)
+        basic.pause(200)
+    }
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . # # # .
+        # . # . #
+        # # # # #
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . # # # .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
     gameField()
     pixelAt(x22, y22)
+    while (cx >= 0 && cy >= 0) {
+        led.plot(cx, cy)
+        basic.pause(1000)
+        cx += -1
+        if (cx < 0) {
+            cx = 4
+            cy += -1
+        }
+    }
+    finished = true
+    basic.showNumber(hit)
 })
